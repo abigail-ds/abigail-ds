fdeed07a3b429c2199889af0bc38d2ce1d7a43be
Nathan Keeyss <keeysnc@gmail.com>
2020-05-19 22:29:58 -0400

    registration form update

--- src/pages/registration.js
+++ src/pages/registration.js
@@ -42,8 +42,8 @@ const Registration = ({ intl }) => {
       />
 
       <Form
-        agreementEn={path.match("/en/registration/") ? data.form.html : " "}
-        agreementEs={path.match("/es/registration/") ? data.formEs.html : " "}
+        agreementEn={path.match("/en/registration/") ? data.form.html : null}
+        agreementEs={path.match("/es/registration/") ? data.formEs.html : null}
         courseTitle={intl.formatMessage({ id: "registration.course_title" })}
         courseInfoTitle={intl.formatMessage({
           id: "registration.student_info_title",
