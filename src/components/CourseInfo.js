25be1da847ad31938fb07edcd90cb58f82116281
Nathan Keeyss <keeysnc@gmail.com>
2021-02-10 16:02:10 -0500

    remove registration buttons

--- src/components/CourseInfo.js
+++ src/components/CourseInfo.js
@@ -6,14 +6,14 @@ const CourseInfo = props => {
   return (
     <div style={{ marginBottom: "20px" }}>
       <div dangerouslySetInnerHTML={{ __html: props.message }} />
-      {props.title === "Driver's Manual Course" ||
+      {/* {props.title === "Driver's Manual Course" ||
       props.title === "Curso Manual de Conductores" ? (
         " "
       ) : (
         <Link to="/registration" className="waves-effect waves-light btn">
           Register
         </Link>
-      )}
+      )} */}
     </div>
   )
 }
